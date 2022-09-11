a381967fe278fb3afbf260f7bfb62367964cdd66
Daniel Murphy <knysnacoder@gmail.com>
2022-09-11 11:45:29 +0200

    Adding a search.vue page

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,32 @@
         </a>
       </div>
       <div class="navbar-menu" id="navbar-menu" :class="{'is-active': showMobileMenu }">
-        <div class="navbar-end">
-          <div class="navbar-end">
-            <router-link to="/summer" class="navbar-item">Summer</router-link>
-            <router-link to="/winter" class="navbar-item">Winter</router-link>
-            <div class="navbar-item">
-              <div class="buttons">
-                <router-link to="/log-in" class="button is-light">Log in</router-link>
-                <router-link to="/cart" class="button is-success">
-                  <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                  <span>Cart ({{cartTotalLength}})</span>
-                </router-link>
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form action="/search" method="get">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+                <div class="control">
+                  <button class="button is-success">
+                    <span class="icon"><i class="fas fa-search"></i></span>
+                  </button>
+                </div>
               </div>
+            </form>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart ({{cartTotalLength}})</span>
+              </router-link>
             </div>
           </div>
         </div>
