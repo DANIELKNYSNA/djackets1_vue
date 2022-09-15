5193cef11e42775a8636f5e8f1a089196c123c04
Daniel Murphy <knysnacoder@gmail.com>
2022-09-15 18:32:52 +0200

    Order summary component

---
+++ src/components/OrderSummary.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-order-summary">
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'OrderSummary',
+  props: {
+    Order: Object
+  },
+  data() {
+    return {
+      items: []
+    }
+  }
+}
+</script>
--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -7,20 +7,32 @@
       <div class="column is-12">
         <button @click="logout()" class="button is-danger">Log Out</button>
       </div>
+      <hr>
+      <div class="column is-12">
+        <h2 class="subtitle">My Orders</h2>
+        <OrderSummary v-for="order in orders" :key="order.id" :order="order" />
+      </div>
     </div>
   </div>
 </template>
 <script>
 import axios from 'axios'
+import OrderSummary from '@components/OrderSummary.vue'
 export default {
   name: 'MyAccount',
+  components: OrderSummary,
   data() {
     return {
       user: {
         name: '',
         email: ''
-      }
+      },
+      order: order
     }
+  },
+  mounted() {
+    document.title = 'My Account | Djackets'
+    this.getMyOrders()
   },
   methods: {
     logout() {
@@ -30,6 +42,18 @@ export default {
       localStorage.removeItem('userid')
       this.$store.commit('removeToken')
       this.$router.push('/')
+    },
+    async getMyOrders() {
+      this.$store.commit("setIsLoading", true)
+      await axios.get('/api/v1/orders/')
+        .then(response => {
+          this.orders = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      this.$store.commit('setIsLoading', false)
+
     }
   }
 
