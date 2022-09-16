6d8f25d24dda75606e99bea952c592fd87b5dee6
Daniel Murphy <knysnacoder@gmail.com>
2022-09-16 17:49:54 +0200

    Adding my account orders summary

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -2,50 +2,54 @@
   <div class="page-my-account">
     <div class="columns is-multiline">
       <div class="column is-12">
-        <h1 class="title">My Account</h1>
+        <h1 class="title">My account</h1>
       </div>
+
       <div class="column is-12">
-        <button @click="logout()" class="button is-danger">Log Out</button>
+        <button @click="logout()" class="button is-danger">Log out</button>
       </div>
+
       <hr>
+
       <div class="column is-12">
-        <h2 class="subtitle">My Orders</h2>
-        <OrderSummary v-for="order in orders" :key="order.id" :order="order" />
+        <h2 class="subtitle">My orders</h2>
+
+        <OrderSummary v-for="order in orders" v-bind:key="order.id" v-bind:order="order" />
       </div>
     </div>
   </div>
 </template>
+
 <script>
 import axios from 'axios'
-import OrderSummary from '@components/OrderSummary.vue'
+import OrderSummary from '@/components/OrderSummary.vue'
 export default {
   name: 'MyAccount',
-  components: OrderSummary,
+  components: {
+    OrderSummary
+  },
   data() {
     return {
-      user: {
-        name: '',
-        email: ''
-      },
-      order: order
+      orders: []
     }
   },
   mounted() {
-    document.title = 'My Account | Djackets'
+    document.title = 'My account | Djackets'
     this.getMyOrders()
   },
   methods: {
     logout() {
       axios.defaults.headers.common["Authorization"] = ""
-      localStorage.removeItem('token')
-      localStorage.removeItem('username')
-      localStorage.removeItem('userid')
+      localStorage.removeItem("token")
+      localStorage.removeItem("username")
+      localStorage.removeItem("userid")
       this.$store.commit('removeToken')
       this.$router.push('/')
     },
     async getMyOrders() {
-      this.$store.commit("setIsLoading", true)
-      await axios.get('/api/v1/orders/')
+      this.$store.commit('setIsLoading', true)
+      await axios
+        .get('/api/v1/orders/')
         .then(response => {
           this.orders = response.data
         })
@@ -53,9 +57,7 @@ export default {
           console.log(error)
         })
       this.$store.commit('setIsLoading', false)
-
     }
   }
-
 }
 </script>
